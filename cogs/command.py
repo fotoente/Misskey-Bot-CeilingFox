f3148a1da8e5e2082d6db183ae858da8a440fe39
yupix <yupi0982@outlook.jp>
2022-01-06 16:24:32 +0900

    feat: add MentioncommandCog

---
+++ cogs/command.py
@@ -0,0 +1,23 @@
+from mi.ext import commands
+from mi import Note
+
+from ceilingfox import ceiling_fox_number, ceiling_fox_yes_no
+from utils import get_mention
+
+
+class MentionCommandCog(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.mention_command(regex=r'!yesno')
+    async def yesno(self, ctx: Note):
+        await ctx.reply(f'{get_mention(ctx)}\n{ceiling_fox_yes_no()}')
+
+    @commands.mention_command(regex=r'!story')
+    async def story(self, ctx: Note):
+        await ctx.reply(f'{get_mention(ctx)}\n{ceiling_fox_number()}')
+
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(MentionCommandCog(bot))
