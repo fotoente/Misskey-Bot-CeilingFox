15a6c42ec119803aa753bf6ca33ea345ed3496ba
yupix <yupi0982@outlook.jp>
2022-01-23 22:44:51 +0900

    chore: replacing the get_mention method

--- cogs/command.py
+++ cogs/command.py
@@ -2,8 +2,6 @@ from mi.ext import commands
 from mi import Note
 
 from ceilingfox import ceiling_fox_number, ceiling_fox_yes_no, ceiling_fox_story
-from utils import get_mention
-
 
 class MentionCommandCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -11,15 +9,15 @@ class MentionCommandCog(commands.Cog):
 
     @commands.mention_command(regex=r'!yesno')
     async def yesno(self, ctx: Note,*args,**kwargs):
-        await ctx.reply(f'{get_mention(ctx)}\n{ceiling_fox_yes_no()}')
+        await ctx.reply(f'{ctx.author.action.get_mention()}\n{ceiling_fox_yes_no()}')
 
     @commands.mention_command(regex=r'!story')
     async def story(self, ctx: Note,*args,**kwargs):
-        await ctx.reply(f'{get_mention(ctx)}\n{ceiling_fox_story()}')
+        await ctx.reply(f'{ctx.author.action.get_mention()}\n{ceiling_fox_story()}')
     
     @commands.mention_command(regex=r'!number')
     async def number(self, ctx: Note,*args,**kwargs):
-        await ctx.reply(f'{get_mention(ctx)}\n{ceiling_fox_number()}')
+        await ctx.reply(f'{ctx.author.action.get_mention()}\n{ceiling_fox_number()}')
 
 
 def setup(bot: commands.Bot):
